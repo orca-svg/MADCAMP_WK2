b1d50f0d9880118205484364d77d85b660932493
LYoooJ <limyoojin@postech.ac.kr>
2026-01-17 18:05:27 +0900

    feat: scaffold project structure and setup Prisma module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { StoriesModule } from './stories/stories.module';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { AdviceModule } from './advice/advice.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, StoriesModule, UsersModule, CommentsModule, AdviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
