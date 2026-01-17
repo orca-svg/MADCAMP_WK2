5479d47d898c4a3f191649c93b47bf0c0bddde5f
LYoooJ <limyoojin@postech.ac.kr>
2026-01-17 12:06:50 +0900

    docs: configure Swagger for backend API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BaseApiDocument } from './swagger.document';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new BaseApiDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
