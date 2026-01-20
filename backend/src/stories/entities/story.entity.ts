8ca836013942c6fad4df86b662f98812bec4b216
LYoooJ <limyoojin@postech.ac.kr>
2026-01-20 12:26:04 +0900

    fix: delete Emotion and add Tag in StoryEntity

--- backend/src/stories/entities/story.entity.ts
+++ backend/src/stories/entities/story.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Story, Emotion } from '@prisma/client';
+import { Story, Tag } from '@prisma/client';
 
 export class StoryEntity implements Story {
     @ApiProperty({ description: '사연 고유 ID', example: '1234' })
@@ -14,12 +14,23 @@ export class StoryEntity implements Story {
     @ApiProperty({ description: '사연 공개 여부', example: true })
     isPublic!: boolean;
 
-    @ApiProperty({ description: 'Emotion associated with the story', example: 'SAD' })
-    emotion: Emotion | null = null;
-
     @ApiProperty({ description: '사연 작성자 ID', example: '1234'})
     userId!: string;
 
     @ApiProperty({ description: '사연 작성일', example: '2023-10-01T12:34:56Z' })
     createdAt!: Date;
+
+    @ApiProperty({ 
+        description: '사연에 연결된 태그 목록', 
+        type: 'array',
+        items: {
+            type: 'object',
+            properties: {
+                id: { type: 'string' },
+                name: { type: 'string' }
+            }
+        },
+        example: [{ id: 'tag-1', name: '#불안 😰' }, { id: 'tag-2', name: '#관계 🤝' }]
+    })
+    tags?: Tag[];
 }
