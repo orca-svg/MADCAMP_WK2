e33259e8cc21b0d362c3e004cb102cd062bab298
LYoooJ <limyoojin@postech.ac.kr>
2026-01-21 12:30:46 +0900

    fix: edit dailymessage

--- frontend/lib/providers/daily_message_provider.dart
+++ frontend/lib/providers/daily_message_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -62,7 +63,10 @@ class DailyMessageController extends StateNotifier<DailyMessageState> {
   Future<void> power() async {
     final today = _todayStamp();
     final storedDate = _prefs.getString(_messageDateKey);
-    final storedId = _prefs.getString(_messageIdKey);
+    final rawId = _prefs.get(_messageIdKey);
+    final storedId = rawId is String
+        ? rawId
+        : (rawId is int ? rawId.toString() : null);
     final storedMessage = _prefs.getString(_messageContentKey);
     final storedSeenAt = _prefs.getString(_messageSeenAtKey);
 
@@ -82,29 +86,39 @@ class DailyMessageController extends StateNotifier<DailyMessageState> {
       return;
     }
 
-    final res = await _client.dio.get('/advice/random');
-    final obj = extractObject(res.data);
-    final id = obj['id']?.toString();
-    final message = (obj['content'] ?? '').toString();
-    if (id == null || id.isEmpty || message.isEmpty) {
-      return;
+    try {
+      debugPrint('dailyMessage baseUrl=${_client.dio.options.baseUrl}');
+      final res = await _client.dio.get('/advice/random');
+      debugPrint('dailyMessage response=${res.data}');
+      final resData = res.data;
+      final obj = (resData is Map && resData['data'] is Map)
+          ? extractObject(resData['data'])
+          : extractObject(resData);
+      final id = obj['id']?.toString();
+      final message = (obj['content'] ?? '').toString();
+      if (id == null || id.isEmpty || message.isEmpty) {
+        return;
+      }
+
+      final seenAt = DateTime.now();
+      _prefs.setString(_messageDateKey, today);
+      _prefs.setString(_messageIdKey, id);
+      _prefs.setString(_messageContentKey, message);
+      _prefs.setString(_messageSeenAtKey, seenAt.toIso8601String());
+
+      state = DailyMessageState(
+        messageId: id,
+        message: message,
+        isRepeat: false,
+        hasTuned: true,
+        hasSeenTodayMessage: true,
+        lastSeenDate: today,
+        firstCheckTime: seenAt,
+      );
+    } catch (e) {
+      debugPrint('dailyMessage error=$e');
+      // Keep state unchanged on error.
     }
-
-    final seenAt = DateTime.now();
-    _prefs.setString(_messageDateKey, today);
-    _prefs.setString(_messageIdKey, id);
-    _prefs.setString(_messageContentKey, message);
-    _prefs.setString(_messageSeenAtKey, seenAt.toIso8601String());
-
-    state = DailyMessageState(
-      messageId: id,
-      message: message,
-      isRepeat: false,
-      hasTuned: true,
-      hasSeenTodayMessage: true,
-      lastSeenDate: today,
-      firstCheckTime: seenAt,
-    );
   }
 
   void resetSession() {
